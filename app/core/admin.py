442f6a1aad6bbc28a9de810e33b7e3e21cf5e237
Sem31 <31kamleshprajapat@gmail.com>
2024-09-16 08:22:18 +0530

    Created user admin, with custom view

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,14 @@
-# from django.contrib import admin
+"""
+User admin view
+"""
+from django.contrib import admin
+from core.models import User
 
-# Register your models here.
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    """User admin view"""
+    list_display = ('email', 'name', 'is_active', 'is_superuser')
+    list_filter = ('is_active', 'is_superuser')
+    search_fields = ('email', 'name')
+    ordering = ('email',)
