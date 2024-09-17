87cde2d2e45b549328e8081e9ce6d2ee51120d2d
Sem31 <31kamleshprajapat@gmail.com>
2024-09-17 07:41:38 +0530

    create user and write testcases

--- app/user/views.py
+++ app/user/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+"""
+Views for the user api
+"""
 
-# Create your views here.
+from rest_framework import generics
+# from rest_framework.views import viewsets
+
+from .serializers import UserSerializer
+
+
+class CreateUserView(generics.CreateAPIView):
+    """Create a new user in the system."""
+    serializer_class = UserSerializer
+
+
+# class UserViewSet(viewsets.ModelViewSet):
+#     """User viewset"""
+#     queryset = get_user_model().objects.all()
+#     serializer_class = UserSerializer
