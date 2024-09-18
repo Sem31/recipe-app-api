c5443f2ce3ac48c39ce034da1813ebc7c0396c5c
Sem31 <31kamleshprajapat@gmail.com>
2024-09-19 05:23:46 +0530

    Right authorized API and created update API

--- app/user/views.py
+++ app/user/views.py
@@ -3,14 +3,38 @@ Views for the user api
 """
 
 from rest_framework import generics
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
+from rest_framework import authentication, permissions
+
 # from rest_framework.views import viewsets
 
-from .serializers import UserSerializer
+from .serializers import UserSerializer, AuthTokenSerializer
 
 
 class CreateUserView(generics.CreateAPIView):
     """Create a new user in the system."""
+
     serializer_class = UserSerializer
+
+
+class CreateTokenView(ObtainAuthToken):
+    """Create a new auth token for user."""
+
+    serializer_class = AuthTokenSerializer
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
+
+
+class ManageUserView(generics.RetrieveUpdateAPIView):
+    """Manage the authenticated user."""
+
+    serializer_class = UserSerializer
+    authentication_classes = [authentication.TokenAuthentication]
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        """Retrieve and return the authenticated user."""
+        return self.request.user
 
 
 # class UserViewSet(viewsets.ModelViewSet):
